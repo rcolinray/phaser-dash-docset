1250fcffe8c929d5dd15f3a1f14c3af9efae33a6
Colin Ray <rcray@iastate.edu>
2014-03-28 01:05:00 -0500

    Added Info.plist. Added init routines to Main.hs. Added phaser submodule. Updated dependencies.

--- Main.hs
+++ Main.hs
@@ -2,17 +2,40 @@
 {-# LANGUAGE ExtendedDefaultRules #-}
 {-# OPTIONS_GHC -fno-warn-type-defaults #-}
 
-import Shelly
+import Shelly (shelly, verbosely, mkdir_p, cp_r, rm_rf, cp)
+import qualified Data.Text.Lazy as LT
+import Database.HDBC
+import Database.HDBC.Sqlite3
 
-import Data.Text.Lazy as LT
 default (LT.Text)
 
--- Create the Docset Folder
--- Copy existing HTML Documentation
--- Create the Info.plist File
--- Create the SQLite index
--- Populate the SQLite index
+initDocset :: IO ()
+initDocset = shelly $ verbosely $ do
+  -- Create the Docset Folder
+  mkdir_p "Phaser.docset/Contents/Resources/"
+
+  -- Copy existing HTML Documentation
+  cp_r "phaser/docs" "Phaser.docset/Contents/Resources/Documents"
+  rm_rf "Phaser.docset/Contents/Resources/Documents/build"
+
+  -- Create the Info.plist File
+  cp "Info.plist" "Phaser.docset/Contents/"
 
 main :: IO ()
-main = shelly $ verbosely $ do
-  mkdir_p "Phaser.docset/Contents/Resources/Documents/"
+main = do
+  initDocset
+
+  -- Create the SQLite index
+  conn <- connectSqlite3 "Phaser.docset/Contents/Resources/docSet.dsidx"
+  handleSql (\_ -> return 0) $ run conn "DROP TABLE searchIndex;" []
+  run conn "CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);" []
+  run conn "CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);" []
+
+  -- Populate the SQLite index
+  -- INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES ('name', 'type', 'path');
+  -- Phaser Namespace
+  -- Phaser Classes
+  -- Each class will have constructors, attributes, and methods
+
+  commit conn
+  disconnect conn
